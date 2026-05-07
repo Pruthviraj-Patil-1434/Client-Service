074b2a034933b54b1b0169d35d086225551fde91
shyamrai123 <raishyamkumar21@gmail.com>
2026-05-07 18:09:09 +0530

    first commit

---
+++ src/main/java/com/example/clientservicedemo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.clientservicedemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Client Service Working";
+    }
+}
---
+++ src/test/java/com/example/clientservicedemo/ClientservicedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientservicedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
